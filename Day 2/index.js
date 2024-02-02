95904b94268bc46298003bdf755a9d2df8d88378
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-02 20:45:55 +0430

    first commit

--- Day 2/index.js
+++ Day 2/index.js
@@ -47,7 +47,7 @@ function main() {
 
     const tip_percent = parseInt(readLine().trim(), 10);
 
-    const tax_percent = parseInt(readLine().trim(), 10);
+    const tax_percent = parseInt(readLine().trim(), 10);  
 
     solve(meal_cost, tip_percent, tax_percent);  
 };        
