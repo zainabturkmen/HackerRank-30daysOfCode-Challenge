a35e74ac9cc117d351276148009d6fa736437b7a
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-02 20:49:22 +0430

    first commit

--- Day 2/index.js
+++ Day 2/index.js
@@ -39,7 +39,7 @@ function solve(meal_cost, tip_percent, tax_percent) {
     // Write your code here the given task tha we solved.
     const totalCost = Math.round(meal_cost + (meal_cost * tip_percent / 100) + (meal_cost * tax_percent / 100) )
  
-console.log(totalCost)           
+console.log(totalCost)              
 };                      
        
 function main() { 
