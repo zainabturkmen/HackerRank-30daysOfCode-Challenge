7a015fd76ae40a0ad61d41ba5438f8fa278b31f3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-02 17:50:51 +0430

    solving the challenge

--- Day 2/index.js
+++ Day 2/index.js
@@ -3,7 +3,7 @@
  tip percent (the percentage of the meal price being added as tip), 
  and tax percent (the percentage of the meal price being added as tax) for a meal, 
  find and print the meal's total cost.Round the result to the nearest integer.
- */
+ */  
 
 //  solving the challenge
 
