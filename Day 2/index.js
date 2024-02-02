0c9884e3bdcee517d0ea262f7a8c841349d90135
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-02 17:49:46 +0430

    solving the challenge

--- Day 2/index.js
+++ Day 2/index.js
@@ -6,3 +6,51 @@
  */
 
 //  solving the challenge
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here the given task tha we solved
+    const totalCost = Math.round(meal_cost + (meal_cost * tip_percent / 100) + (meal_cost * tax_percent / 100) )
+
+console.log(totalCost)
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine().trim());
+
+    const tip_percent = parseInt(readLine().trim(), 10);
+
+    const tax_percent = parseInt(readLine().trim(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
