977e48987bf71a8cd3abd4da3f8509b8e7c97ea5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-02 18:01:24 +0430

    first commit

--- Day 2/index.js
+++ Day 2/index.js
@@ -36,7 +36,6 @@ function readLine() {
  *  2. INTEGER tip_percent
  *  3. INTEGER tax_percent
  */  
-
 function solve(meal_cost, tip_percent, tax_percent) {
     // Write your code here the given task tha we solved
     const totalCost = Math.round(meal_cost + (meal_cost * tip_percent / 100) + (meal_cost * tax_percent / 100) )
