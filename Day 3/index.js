7e21211a72aad92a590b0b691374b18f3f2e6aed
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-05 09:28:34 +0430

    day 3 challenge

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,4 +1,11 @@
 // Day 3: Intro to Conditional Statements
 
 /* Task
+Given an integer,n, perform the following conditional actions:
+
+If n is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+
 */
