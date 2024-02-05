297dd86569d2ba8c45deb7203331cc612cd3a24b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-05 13:15:28 +0430

    day 5 challenge

---
+++ Day 5/index.js
@@ -0,0 +1,33 @@
+// 
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    for (let i =1; i <= 10; i++){
+        const result = `${n} x ${i} = ${n * i}`
+        console.log(result)
+    }
+}
