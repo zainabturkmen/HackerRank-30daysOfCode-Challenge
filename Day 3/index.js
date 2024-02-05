4adc55da556819dacf494c0e1dcdf883c0f80ab6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-05 09:29:37 +0430

    day 3 challenge

--- Day 3/index.js
+++ Day 3/index.js
@@ -4,8 +4,8 @@
 Given an integer,N, perform the following conditional actions:
 
 If N is odd, print Weird
-If N is even and in the inclusive range of  to , print Not Weird
-If N is even and in the inclusive range of  to , print Weird
+If N is even and in the inclusive range of 2 to 5, print Not Weird
+If N is even and in the inclusive range of 6 to , print Weird
 If N is even and greater than , print Not Weird
 
 */
