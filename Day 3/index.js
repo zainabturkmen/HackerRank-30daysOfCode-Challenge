d3b7febad137da89b4930fb6e2f65888f75e87d9
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-05 09:30:10 +0430

    day 3 challenge

--- Day 3/index.js
+++ Day 3/index.js
@@ -7,3 +7,4 @@ If N is even and in the inclusive range of 2 to 5, print Not Weird
 If N is even and in the inclusive range of 6 to 20, print Weird
 If N is even and greater than 20, print Not Weird
 */
+
