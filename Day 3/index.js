9f2c7c0634abfd2992926854279f8a586bf1b1f7
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-05 09:30:46 +0430

    day 3 challenge

--- Day 3/index.js
+++ Day 3/index.js
@@ -8,3 +8,35 @@ If N is even and in the inclusive range of 6 to 20, print Weird
 If N is even and greater than 20, print Not Weird
 */
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Write your code here
+
+function main() {
+    const N = parseInt(readLine().trim(), 10);
+    if (N % 2 !==0 || (N >= 6 && N<= 20)){
+        console.log("Weird")
+    } else {
+        console.log("Not Weird")
+    }
+}
